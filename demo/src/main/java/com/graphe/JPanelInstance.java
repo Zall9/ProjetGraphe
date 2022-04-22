167a3ce3a27d5f78c30883c72bfedba2a5c69be4
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2022-04-22 15:23:21 +0200

    JPanelInstance fait

--- demo/src/main/java/com/graphe/JPanelInstance.java
+++ demo/src/main/java/com/graphe/JPanelInstance.java
@@ -1,5 +1,34 @@
 package com.graphe;
 
-public class JPanelInstance {
-    
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.awt.*;
+import javax.swing.*;
+
+class JPanelAttribut extends JPanel {
+
+    private JLabel instanceLabel;
+    private JTextField textfieldInput;
+    public JPanelAttribut() {
+        super();
+        GridLayout grid = new GridLayout(0, 2);
+        setLayout(grid);
+        //JTextField textfieldInput= new JTextField("Entrez la valeur de l'attribut");
+        textfieldInput.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTextField tf = (JTextField) e.getSource();
+                System.out.println(tf.getText());
+            }
+        });
+
+        // Initialisation du label du type de concept
+        instanceLabel = new JLabel("Valeur:  ");
+
+        // Ajout des composants au panel
+        this.add(instanceLabel);
+        this.add(textfieldInput);
+    }
+
 }
