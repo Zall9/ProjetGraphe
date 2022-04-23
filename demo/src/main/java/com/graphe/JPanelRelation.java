c2ff6710cb840e0716343542c3fa90d588437d1d
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2022-04-23 15:50:50 +0200

    fix: JPanelRelation visible

--- demo/src/main/java/com/graphe/JPanelRelation.java
+++ demo/src/main/java/com/graphe/JPanelRelation.java
@@ -1,6 +1,7 @@
 package com.graphe;
 
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 import java.awt.*;
@@ -20,18 +21,14 @@ public class JPanelRelation extends JPanel{
         JComboBox<String> comboNoeudDroit=new JComboBox<String>();
         JComboBox<String> comboRelation=new JComboBox<String>();
         System.out.println("prout0");
-        panelRelation = new JPanel();
-        panelRelation.setLayout(layout);
+        setLayout(layout);
         System.out.println("prout1");
         this.grapheLogique = grapheLogique;
         System.out.println("prout");
-        //ArrayList<Noeud> listeNoeuds = new ArrayList<Noeud>();
-        //listeNoeuds = (ArrayList<Noeud>) grapheLogique.getGraphe();
-        
         for (Noeud n : (ArrayList<Noeud>) grapheLogique.getGraphe()) {
             
-            comboNoeudGauche.addItem(n.getId());
-            comboNoeudDroit.addItem(n.getId());
+            comboNoeudGauche.addItem(n.toString());
+            comboNoeudDroit.addItem(n.toString());
         }
         for (TypeRelation rel : TypeRelation.values()) {
             comboRelation.addItem(rel.toString());
@@ -39,10 +36,16 @@ public class JPanelRelation extends JPanel{
         AutoCompleteDecorator.decorate(comboNoeudGauche);
         AutoCompleteDecorator.decorate(comboNoeudDroit);
         AutoCompleteDecorator.decorate(comboRelation);
-
-        panelRelation.add(comboNoeudGauche);
-        panelRelation.add(comboRelation);
-        panelRelation.add(comboNoeudDroit);
+        
+        JLabel noeud1 = new JLabel("Noeud : ");
+        JLabel relationLabel = new JLabel("Relation: ");
+        JLabel noeud2 = new JLabel("Noeud : ");
+        this.add(noeud1);
+        this.add(comboNoeudGauche);
+        this.add(relationLabel);
+        this.add(comboRelation);
+        this.add(noeud2);
+        this.add(comboNoeudDroit);
     }
 }
 
